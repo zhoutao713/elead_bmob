f95c1e70c15e42e5f6df56b1cf773d3591305f3d
zhoutao713 <37416651@qq.com>
2015-03-09 22:24:26 +0800

    ALM app 第一版本

---
+++ Elead_Bmob/src/com/bmob/im/demo/config/Config.java
@@ -0,0 +1,12 @@
+package com.bmob.im.demo.config;
+
+/** 系统变量
+  * @ClassName: Config
+  * @Description: TODO
+  * @author smile
+  * @date 2014-6-17 上午9:40:11
+  */
+public class Config {
+
+	public static String applicationId = "297b34983d22d96fb7a63721c7e0b3dc";
+}
